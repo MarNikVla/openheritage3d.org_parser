03b812ae2baf44f4d5d705d6e7e809a1faf2a15f
marnikvla <condor52@yandex.ru>
2021-07-21 22:14:06 +0300

    cleaning

--- csv_tools.py
+++ csv_tools.py
@@ -1,7 +1,4 @@
 import csv
-import functools
-import itertools
-import shutil
 from typing import Dict
 from pathlib import Path
 
@@ -43,44 +40,8 @@ def csv_writer(data: Dict[str, str]):
             writer.writeheader()
         writer.writerow(data)
 
-@functools.lru_cache(maxsize=32)
-def csv_to_list():
-    list_of_proj_from_csv = list()
-    with open(path_to_file, 'r', newline='', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile, fieldnames=csv_columns)
-        for row in reader:
-            list_of_proj_from_csv.append((row['project_name'], row['status']))
-        return list_of_proj_from_csv
 
-
-def csv_compare(dict_from_url):
-    list_of_proj_from_csv = csv_to_list()
-    with open(path_to_file, 'r') as csvfile, open('output.csv', 'w') as outputfile:
-        reader = csv.DictReader(csvfile, fieldnames=csv_columns)
-        writer = csv.DictWriter(outputfile, fieldnames=csv_columns)
-
-
-        if (dict_from_url['project_name'], dict_from_url['status']) in list_of_proj_from_csv:
-            print('ffff')
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-            if not is_file_exist(outputfile):
-                writer.writeheader()
-            writer.writerow(dict_from_url)
-        print(list_of_proj_from_csv)
-                # writer.writerow({'first_name': row['first_name'], 'number': row['number']})
-    # shutil.move('output.csv', 'names.csv')
-
-
-# with open('names.csv', 'r') as csvfile, open('output.csv', 'w') as outputfile:
-#     reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-#     writer = csv.DictWriter(outputfile, fieldnames=fieldnames)
-#     for row in reader:
-#         if not name == row['first_name']:
-#             writer.writerow({'first_name': row['first_name'], 'number': row['number']})
-# shutil.move('output.csv','names.csv')
-
-if __name__ == '__main__':
-    from openheritage_parser import parse_single_project
-
+# if __name__ == '__main__':
+#     from openheritage_parser import parse_single_project
     # csv_writer(parse_single_project('https://openheritage3d.org/project.php?id=n3kf-7713'))
-    # csv_compare(parse_single_project('https://openheritage3d.org/project.php?id=05r8-we91'))
+
--- main.py
+++ main.py
@@ -7,26 +7,21 @@ from codetiming import Timer
 Timer = Timer()
 OPENHERITAGE_DATA_HTML = 'https://openheritage3d.org/data'
 
-# csv_writer(parse_single_project('https://openheritage3d.org/project.php?id=5b6m-ap62'))
-# csv_writer(parse_single_project('https://openheritage3d.org/project.php?id=ws0a-3g91'))
-# print(parse_data_page(OPENHERITAGE_DATA_HTML)[:3])
 data = parse_data_page(OPENHERITAGE_DATA_HTML)
 
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
-
 ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.DEBUG)
 
 Timer.start()
-for n,i in enumerate(range(len(data))):
-
+for n, i in enumerate(range(len(data))):
     try:
         csv_writer(parse_single_project(data[i]))
     except AttributeError as e:
         print(f'Exception {e} in url {data[i]}')
 
-    formatter = logging.Formatter(f'%(asctime)s - {n}/{len(data)} - {data[i]}')
+    formatter = logging.Formatter(f'%(asctime)s - {n+1}/{len(data)} - {data[i]} - %(message)s')
     ch.setFormatter(formatter)
     root.addHandler(ch)
 
